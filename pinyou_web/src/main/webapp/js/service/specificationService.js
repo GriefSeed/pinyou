14bedc5cdacffec9f59881ede7093fb8c6bbdce6
css <shusen2013@outlook.com>
2018-09-08 09:06:13 +0800

    使用AngularJS的service层重构品牌管理模块

---
+++ pinyou_web/src/main/webapp/js/service/specificationService.js
@@ -0,0 +1,17 @@
+app.service("specificationService",function($http){
+    this.search=function(pageNum,pageSize,entity){
+        return $http.post("../specification/search?pageNum="+pageNum+"&pageSize="+pageSize,entity);
+    }
+    this.add=function(entity){
+        return $http.post("../specification/add",entity);
+    }
+    this.findOne=function(id){
+        return $http.get("../specification/findOne?id="+id);
+    }
+    this.update=function(entity){
+        return $http.post("../specification/update",entity);
+    }
+    this.dele=function(ids){
+        return $http.get("../specification/delete?ids="+ids);
+    }
+})
